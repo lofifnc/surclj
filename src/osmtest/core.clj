8fa0d7d40646388a571b61c38a80257c5a72e94b
Sören Gutzeit <soeren.gutzeit@mni.thm.de>
2014-12-13 12:47:36 +0100

    document code

--- src/osmtest/core.clj
+++ src/osmtest/core.clj
@@ -4,26 +4,47 @@
 
 
 
+; TODO read Data File (and maybe pictures)
 
+; TODO request
+
+; TODO here use response instead of fix file
 (def xml (xml/parse  "resources/schwimmbad.osm"))
 
 
-(defn childsByTag [tag xml] (filter #(= tag (get % :tag)) (get xml :content)))
 
+
+
+(defn childsByTag
+  "give a sequence of xml child item with the given tag from the given root xml"
+  [tag xml]
+  (filter #(= tag (get % :tag)) (get xml :content)))
+
+; nodes of file
 (def nodes (childsByTag :node xml))
+
+; ways of file
 (def ways (childsByTag :way xml))
 
 
+
+
+
+
 (defn circle? [way]
+  "is this way a closed circle?"
   (let [nodes (childsByTag :nd way)]
         (= (get (get (first nodes) :attrs) :ref) (get (get (last nodes) :attrs) :ref))))
 
+(circle? (last (take 5 ways)))
 
-(circle? (last (take 3 ways)))
+
+
 
 
 
 (defn nodesByWay [way]
+  "give a sequence of nodes that are used in the given way"
   (let [nodeInWayRefs (fn [node refs]
           (if (= (get (get node :attrs) :id) (get (get (first refs) :attrs) :ref))
             true
@@ -32,13 +53,14 @@
               (recur node (rest refs)))))]
     (filter #(nodeInWayRefs % (childsByTag :nd way)) nodes)))
 
-
-
 (nodesByWay (first ways))
 
 
 
+
+
 (defn checkWay
+  "check tags of given way by condition function (fn[tag])"
   [way condFn]
   (let
     [checkTag (fn[tags]
@@ -51,14 +73,22 @@
 
 
 
+
+
 (defn wayHasName?
+  "example condition function for checkWay"
   [way]
   (checkWay way (fn[tag] (= "name" (get ( get tag :attrs ) :k)))))
 
 (map nodesByWay (filter #(and (circle? %) (wayHasName? %)) ways))
 
 
+
+
+
+
 (defn isSwimmmingSport?
+  "example condition function for checkWay"
   [way]
   (checkWay way (fn[tag] (and (= "sport" (get ( get tag :attrs ) :k)) (= "swimming" (get ( get tag :attrs ) :v))))))
 
@@ -66,7 +96,7 @@
 (map nodesByWay (filter #(and (isSwimmmingSport? %)) ways))
 
 
-
+; TODO parse result of checkWay-functions to xml structure for kml resuklt file
 
 
 
