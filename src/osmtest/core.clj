959e3ff494dc5a956828c6de36a5f0c940fd4649
Sören Gutzeit <soeren.gutzeit@mni.thm.de>
2014-12-15 09:25:07 +0100

    moved osm functions to own clj file

--- src/osmtest/core.clj
+++ src/osmtest/core.clj
@@ -1,7 +1,6 @@
-(ns osmtest.core
- (require [clojure.xml :as xml]
-          [clojure.zip :as zip]))
+(ns osmtest.core)
 
+(load "osm_parser")
 
 
 ; TODO read Data File (and maybe pictures)
@@ -9,97 +8,42 @@
 ; TODO request
 
 ; TODO here use response instead of fix file
-(def xml (xml/parse  "resources/schwimmbad.osm"))
-
-
-
-
-
-(defn childsByTag
-  "give a sequence of xml child item with the given tag from the given root xml"
-  " @author sören"
-  [tag xml]
-  (filter #(= tag (get % :tag)) (get xml :content)))
+(def xml (parseXml "resources/schwimmbad.osm"))
 
 ; nodes of file
-(def nodes (childsByTag :node xml))
+(def nodes (childsByTag xml :node))
 
 ; ways of file
-(def ways (childsByTag :way xml))
+(def ways (childsByTag xml :way))
+
+(childsByTag (first ways) :nd )
 
 
+(circle? (last (take 3 ways)))
 
+(def nodesByMyWay (nodesByWayCurry nodes))
 
-
-
-(defn circle? [way]
-  "is this way a closed circle?"
-  " @author sören"
-  (let [nodes (childsByTag :nd way)]
-        (= (get (get (first nodes) :attrs) :ref) (get (get (last nodes) :attrs) :ref))))
-
-(circle? (last (take 5 ways)))
-
-
-
-
-
-
-(defn nodesByWay [way]
-  "give a sequence of nodes that are used in the given way"
-  " @author sören"
-  (let [nodeInWayRefs (fn [node refs]
-          (if (= (get (get node :attrs) :id) (get (get (first refs) :attrs) :ref))
-            true
-            (if (empty? (rest refs))
-              false
-              (recur node (rest refs)))))]
-    (filter #(nodeInWayRefs % (childsByTag :nd way)) nodes)))
-
-(nodesByWay (first ways))
-
-
-
-
-
-(defn checkWay
-  "check tags of given way by condition function (fn[tag])"
-  " @author sören"
-  [way condFn]
-  (let
-    [checkTag (fn[tags]
-                (if (condFn (first tags))
-                    true
-                    (if (empty? (rest tags))
-                      false
-                      (recur (rest tags)))))]
-    (checkTag (childsByTag :tag way))))
-
-
+(nodesByMyWay (first ways))
 
 
 
 (defn wayHasName?
-  "example condition function for checkWay"
-  " @author sören"
+  "example condition function for checkWay
+  @author sören"
   [way]
   (checkWay way (fn[tag] (= "name" (get ( get tag :attrs ) :k)))))
 
-(map nodesByWay (filter #(and (circle? %) (wayHasName? %)) ways))
-
-
-
-
+(map nodesByMyWay (filter #(and (circle? %) (wayHasName? %)) ways) )
 
 
 (defn isSwimmmingSport?
-  "example condition function for checkWay"
-  " @author sören"
+  "example condition function for checkWay
+  @author sören"
   [way]
   (checkWay way (fn[tag] (and (= "sport" (get ( get tag :attrs ) :k)) (= "swimming" (get ( get tag :attrs ) :v))))))
 
 
-(map nodesByWay (filter #(and (isSwimmmingSport? %)) ways))
+(map nodesByMyWay (filter #(and (isSwimmmingSport? %)) ways) )
 
 
 ; TODO parse result of checkWay-functions to xml structure for kml resuklt file
---
+++ src/osmtest/osm_parser.clj
@@ -0,0 +1,65 @@
+(ns osmtest.core
+   (require [clojure.xml :as xml]
+          [clojure.zip :as zip]))
+
+(defn parseXml
+  "parse an incoming string into a sequence by xml structure"
+  [str]
+  (xml/parse str))
+
+
+(defn childsByTag
+  "give a sequence of xml child item with the given tag from the given root xml
+  @author sören"
+  [ xml tag ]
+  (filter #(= tag (get % :tag)) (get xml :content)))
+
+
+
+(defn circle?
+  "is this way a closed circle?
+  @author sören"
+  [ way ]
+  (let [nodes (childsByTag way :nd )]
+        (= (get (get (first nodes) :attrs) :ref) (get (get (last nodes) :attrs) :ref))))
+
+
+
+(defn nodesByWay
+  "give a sequence of nodeSet that are used in the given way
+  @author sören"
+  [ nodeSet way ]
+  (let [nodeInWayRefs (fn [node refs]
+          (if (= (get (get node :attrs) :id) (get (get (first refs) :attrs) :ref))
+            true
+            (if (empty? (rest refs))
+              false
+              (recur node (rest refs)))))]
+    (filter #(nodeInWayRefs % (childsByTag way :nd)) nodeSet)))
+
+
+(defn nodesByWayCurry
+  ""
+  [nodeSet]
+  (fn [way]
+    (let [nodeInWayRefs (fn [node refs]
+          (if (= (get (get node :attrs) :id) (get (get (first refs) :attrs) :ref))
+            true
+            (if (empty? (rest refs))
+              false
+              (recur node (rest refs)))))]
+      (filter #(nodeInWayRefs % (childsByTag way :nd)) nodeSet))))
+
+
+(defn checkWay
+  "check tags of given way by condition function (fn[tag])
+  @author sören"
+  [way condFn]
+  (let
+    [checkTag (fn[tags]
+                (if (condFn (first tags))
+                    true
+                    (if (empty? (rest tags))
+                      false
+                      (recur (rest tags)))))]
+    (checkTag (childsByTag way :tag))))
