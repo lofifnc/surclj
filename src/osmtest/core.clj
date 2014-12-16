d62971231dad81a0b20550b2492506246db431ec
Sören Gutzeit <soeren.gutzeit@mni.thm.de>
2014-12-16 13:27:50 +0100

    all in one function for 'startpoint -> ... -> coords of area'

--- src/osmtest/core.clj
+++ src/osmtest/core.clj
@@ -8,6 +8,43 @@
 
 (def startPoint [ 5.34977 50.9348])
 
+
+(defn doLogic
+  "takes coords of startpoint and multiplies the coords by 1.00001 or 0.99999 to get
+  a bounding box arround it. by using this bb, a request get all ways and nodes in this
+  bb. this ways get filtert (see comment) and the coords of the most nearst way to the startPoint will be returned.
+  if there is no fitting way in this bb, this function will be called recursived with a bigger bb."
+  ([startPoint decFactor incFactor]
+   (let [ xml         (parseXml (request (* decFactor (first startPoint))
+                                         (* decFactor (last startPoint))
+                                         (* incFactor (first startPoint))
+                                         (* incFactor (last startPoint))))
+          nodes       (childsByTag xml :node)
+          ways        (filter #(and (circle? %) ;; hier könnten unsere Regeln stehen
+                                    ) (childsByTag xml :way))
+          nodesByWay  (nodesByWayCurry nodes)]
+    (def found (wayCoords nodes (getNearestAreaToPoint ways nodes startPoint)))
+     (if (empty? found)
+       (recur startPoint (* decFactor decFactor) (* incFactor incFactor))
+       found)))
+  ([startPoint]
+   (let [  decFactor   0.99999
+           incFactor   1.00001]
+      (doLogic startPoint decFactor incFactor))))
+
+(doLogic startPoint)
+
+
+
+
+
+
+
+;;
+;; ab hier nur noch beispiele für aufrufe!!!
+;;
+;;
+
 (def decFactor 0.99999)
 (def incFactor 1.00001)
 
@@ -62,6 +99,10 @@
 (wayTags fw)
 (wayCoords nodes fw)
 ; TODO parse result of checkWay-functions to xml structure for kml resuklt file
+
+
+
+
 
 
 
