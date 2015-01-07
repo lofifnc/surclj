2a69420b1c677f8b98bc65476c45764b111875ac
alexander <willi@zeniba.fritz.box>
2015-01-07 16:32:08 +0100

    added factual geo dependency

--- project.clj
+++ project.clj
@@ -3,4 +3,4 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.6.0"][org.clojure/math.numeric-tower "0.0.4"]])
+  :dependencies [[org.clojure/clojure "1.6.0"][org.clojure/math.numeric-tower "0.0.4"][factual/geo "1.0.0"]])
