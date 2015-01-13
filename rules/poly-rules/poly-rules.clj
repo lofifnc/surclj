77df7ec3e8a30c67dd17fee0cbec8412fe8eed0c
mhzg81 <mario.herzog@mni.thm.de>
2015-01-13 20:08:27 +0100

    Poly rules erweitert

--- rules/poly-rules/poly-rules.clj
+++ rules/poly-rules/poly-rules.clj
@@ -1,7 +1,24 @@
-{
- "smoking" true
- "swimming" true
- "parking" true
- "open_fire" true
- "access" false
- }
+{ "smoking" true
+  "swimming" true
+  "parking" true
+  "open_fire" true
+  "noise" true
+  "littering" true
+  "food" true
+  "fishing" true
+  "dog_waste" true
+  "cellphone" true
+  "camera" true
+  "animal_feeding" true
+  "alcohol_consum" true ;????
+    
+  "access" false
+  ; ??? überprüfen
+  "access:skate" true
+  "access:motorvehicel" true
+  "access:dog" true
+  "access:fire_case" true
+  "access:car:lpg" true
+  "access:bicycle" true
+  "access:age" true
+}
