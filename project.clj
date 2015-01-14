79fcd8492171a57d59a85e7f87fe0bca82b29474
Sören Gutzeit <soeren.gutzeit@mni.thm.de>
2015-01-14 21:31:04 +0100

    insert short description

--- project.clj
+++ project.clj
@@ -1,5 +1,5 @@
 (defproject osmtest "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
+  :description "this project is made for informatiCup 2015"
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
