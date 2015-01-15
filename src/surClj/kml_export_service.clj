d8245195cdc02bb47514b74ca1087d4ff3ebed6f
Marco Schaefer <gogomsfr@gmail.com>
2015-01-15 18:49:45 +0100

    fixed double-file bug in kml_export_service.clj

--- src/surClj/kml_export_service.clj
+++ src/surClj/kml_export_service.clj
@@ -5,8 +5,6 @@
 
 (defn to-coordinates-str [coords]
   "convert incoming list of vectors to strings"
-  ;mario ersetzt #(string/join "," %) mit  #(str (string/join "0," %) "0") zum Testen gegen
-  ; die truth.kml Da stehen halt 0er am Ende, kann final wieder geändert werden
   (string/join  "\n" (map #(str (string/join "0," %) "0") coords)))
 
 (defn kml-tmpl [filename coords-str]
@@ -48,10 +46,3 @@
 (defn write-kml [filename coordinates]
   "Write kml file with name <filename> for coordinates <coordinates>"
   (spit (str filename ".computed.kml") (with-out-str (clojure.xml/emit (kml-tmpl filename (to-coordinates-str coordinates))))))
-
-
-(def coordinates-in
-  '(["5.3506056" "50.9344353"] ["5.3508901" "50.9345318"] ["5.3511934" "50.9345868"] ["5.3510527" "50.9346006"] ["5.3512893" "50.9346879"] ["5.3513196" "50.9347198"] ["5.3497853" "50.9347811"] ["5.3512742" "50.9348326"] ["5.3498475" "50.9348581"] ["5.3500446" "50.9350967"] ["5.3502773" "50.9353510"] ["5.3510358" "50.9354012"] ["5.3506481" "50.9354423"] ["5.3503873" "50.9355158"] ["5.3502677" "50.9353366"] ["5.3497302" "50.9347074"] ["5.3507889" "50.9343783"] ["5.3513908" "50.9349383"] ["5.3511612" "50.9354182"] ["5.3507947" "50.9355094"] ["5.3497996" "50.9348001"])
-)
-(map #(str %1 "0") coordinates-in)
-(write-kml "0011" coordinates-in)
